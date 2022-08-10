c6b06ab0220430a12e432b0fd0a454a6334af49d
Tommy Huang <htommyf12@gmail.com>
2022-08-11 02:06:10 +0800

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# Indigo
+
+## Setup
+
+You will need a system with CMake set up and have suitable compilers installed (GCC and Clang are tested)
+
+To build the project, run the following command. You can add other CMake flags as you see fit
+
+```sh
+cmake -B <builddir> .
+cmake --build <builddir>
+```
+
+Alternatively, you can use the [CMake Tools Extension](https://marketplace.visualstudio.com/items?itemName=ms-vscode.cmake-tools) from VSCode and have it configure everything for you
+
+## Contribute
---
+++ src/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+int main(int, char **) {
+    glewExperimental = true;
+    if (!glfwInit()) {
+        std::cerr << "Failed to Initialize GLFW\n";
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window = glfwCreateWindow(1024, 768, "Placeholder", NULL, NULL);
+
+    if (!window) {
+        std::cerr << "Failed to Create Window\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    glewExperimental = true;
+    if (glewInit() != GLEW_OK) {
+        std::cerr << "Failed to Initialize GLEW\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+    do {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    } while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+             glfwWindowShouldClose(window) == 0);
+}
