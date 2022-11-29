44cfd5da23ea9bf7f27d7c6966d304cf63e62d51
noobdy <htommyf12@gmail.com>
2022-11-29 11:37:37 +0800

    added id in trace log

--- src/vertex_buffer.cpp
+++ src/vertex_buffer.cpp
@@ -4,25 +4,25 @@
 
 VertexBuffer::VertexBuffer(const std::vector<float> &vertices,
                            const unsigned int typeSize) {
-    LOG_TRACE("Creating Vertex Buffer");
     m_ComponentCount = typeSize / sizeof(float);
     m_Type = GL_FLOAT;
     glCreateBuffers(1, &m_BufferID);
     glNamedBufferData(m_BufferID, vertices.size() * sizeof(float),
                       vertices.data(), GL_STATIC_DRAW);
+    LOG_TRACE("Creating Vertex Buffer {}", m_BufferID);
 }
 
 VertexBuffer::~VertexBuffer() {
-    LOG_TRACE("Deleting Vertex Buffer");
+    LOG_TRACE("Deleting Vertex Buffer {}", m_BufferID);
     glDeleteBuffers(1, &m_BufferID);
 }
 
 void VertexBuffer::Bind() const {
-    LOG_TRACE("Binding Vertex Buffer");
+    LOG_TRACE("Binding Vertex Buffer {}", m_BufferID);
     glBindBuffer(GL_ARRAY_BUFFER, m_BufferID);
 }
 
 void VertexBuffer::Unbind() const {
-    LOG_TRACE("Unbinding Vertex Buffer");
+    LOG_TRACE("Unbinding Vertex Buffer {}", m_BufferID);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
