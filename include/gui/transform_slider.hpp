c5b66719940214fde9e7af639554b84f6dbc9937
noobdy <htommyf12@gmail.com>
2023-03-13 15:11:54 +0800

    experimental new gui structure

---
+++ include/gui/transform_slider.hpp
@@ -0,0 +1,15 @@
+#ifndef GUI_TRANSFORM_SLIDER_HPP
+#define GUI_TRANSFORM_SLIDER_HPP
+
+#include "pch.hpp"
+
+#include "transform.hpp"
+
+namespace GUI {
+class TransformSlider {
+public:
+private:
+};
+} // namespace GUI
+
+#endif
